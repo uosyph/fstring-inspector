62fb3216c644787913e0b19fcc36ec4b5ce39304
Yousef Saeed <uosyph@gmail.com>
2024-12-08 09:37:17 +0200

    Update description

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    description = f.read()
+
 setup(
     name="fstring_inspector",
-    version="0.1",
+    version="0.1.1",
     packages=find_packages(),
+    long_description=description,
+    long_description_content_type="text/markdown",
 )
