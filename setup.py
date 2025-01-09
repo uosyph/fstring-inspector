f348bfc499a56ba1b9a7271c5cba0be0a19a7174
Yousef Saeed <uosyph@gmail.com>
2025-01-09 05:49:55 +0200

    Update setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup, find_packages
 
 with open("README.md", "r") as f:
-    description = f.read()
+    long_description = f.read()
 
 setup(
     name="fstring_inspector",
     version="0.1.1",
-    packages=find_packages(),
-    long_description=description,
+    author="Yousef Saeed",
+    author_email="uosyph@gmail.com",
+    description="A tool to analyze and identify issues with f-strings in Python code, ensuring proper formatting, usage, and preventing errors.",
+    long_description=long_description,
     long_description_content_type="text/markdown",
+    url="https://github.com/uosyph/fstring-inspector",
+    project_urls={
+        "Bug Tracker": "https://github.com/uosyph/fstring-inspector/issues",
+        "Documentation": "https://github.com/uosyph/fstring-inspector/blob/main/README.md",
+        "Source Code": "https://github.com/uosyph/fstring-inspector",
+    },
+    packages=find_packages(),
+    python_requires=">=3.6",
 )
