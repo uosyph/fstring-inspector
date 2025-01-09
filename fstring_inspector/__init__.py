5363507882432a03861f8cc73eb4b2420c5e9b1c
Yousef Saeed <uosyph@gmail.com>
2025-01-09 06:20:34 +0200

    Import inspect directory function

--- fstring_inspector/__init__.py
+++ fstring_inspector/__init__.py
@@ -1,1 +1,3 @@
-from .main import inspect_fstring, inspect_file
+from .main import inspect_fstring, inspect_file, inspect_directory
+
+__all__ = ["inspect_fstring", "inspect_file", "inspect_directory"]
